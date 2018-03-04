f1ff363111e33f70b695c54f78258ca1dbe5a32b
ArioShaman <ariosha6@gmail.com>
2018-03-04 18:53:50 +0300

    Genre

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180102093854) do
+ActiveRecord::Schema.define(version: 20180304154754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 20180102093854) do
     t.datetime "updated_at", null: false
     t.index ["country_id"], name: "index_country_play_lists_on_country_id"
     t.index ["track_id"], name: "index_country_play_lists_on_track_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "import_csvs", force: :cascade do |t|
--- spec/factories/import_csvs.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :import_csv do
-    
-  end
-end
