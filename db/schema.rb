ddbe51db9e0996c029959ef0a72db2953d1630d6
root <root@midnigthtown.com>
2018-03-20 08:25:06 -0400

    server changes

--- db/schema.rb
+++ db/schema.rb
@@ -15,29 +15,6 @@ ActiveRecord::Schema.define(version: 20180310144801) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "authors", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "countries", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "country_play_lists", force: :cascade do |t|
-    t.bigint "track_id", null: false
-    t.integer "score", default: 0
-    t.bigint "country_id", null: false
-    t.string "year", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["country_id"], name: "index_country_play_lists_on_country_id"
-    t.index ["track_id"], name: "index_country_play_lists_on_track_id"
-  end
-
   create_table "genres", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -55,12 +32,6 @@ ActiveRecord::Schema.define(version: 20180310144801) do
     t.index ["pack_id"], name: "index_images_on_pack_id"
   end
 
-  create_table "import_csvs", force: :cascade do |t|
-    t.string "csv"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "packs", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
@@ -69,16 +40,6 @@ ActiveRecord::Schema.define(version: 20180310144801) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["genre_id"], name: "index_packs_on_genre_id"
-  end
-
-  create_table "tracks", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "cover"
-    t.string "link", null: false
-    t.bigint "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_tracks_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
